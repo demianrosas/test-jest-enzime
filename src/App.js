9dbcc27fc258eb73d34f7b248b2fbde87b0e6ea6
Demian Rosas <drosas@intouchhealth.com>
2019-02-13 08:43:01 -0800

    Trying to figure out how to use codecov

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React By Demian Rosas
+            Learn React By Demito
           </a>
         </header>
       </div>
